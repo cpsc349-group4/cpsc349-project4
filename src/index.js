6e212b2444cfa4a53d0d4552eb9cff338f909c45
David Bonnaud <dbonnaud123@gmail.com>
2021-08-07 22:02:21 -0700

    finished up follow and liking functionality, added follow button and styled

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ async function getUserById (userid) {
     .then((res) => res.json())
     .then((data) => {
       for (const key in data.resources) {
-         console.log(data.resources[key].id)
+        // console.log(data.resources[key].id)
         if (data.resources[key].id == userid) {
           return data.resources[key].username
         }
@@ -48,7 +48,7 @@ async function getUserById (userid) {
     })
 }
 
-async function getUserbyName (username) {
+async function getUserByName (username) {
   return fetch(`http://localhost:5000/users/?username=${username}`, { method: 'get'})
   .then((res) => res.json())
   .then((json) => {
@@ -63,13 +63,14 @@ async function getUserbyName (username) {
   })
 }
 
-async function IsFollowing (userid) {
-  const currentUserId = window.sessionStorage.getItem('user_id')
-  return fetch(`http://localhost:5000/followers/?follower_id=${currentUserId}&following_id=${userid}`, { method: 'get' })
+async function isFollowing (followUserId, userid) {
+  return fetch(`http://localhost:5000/followers/`, { method: 'get' })
   .then((res) => res.json())
   .then((json) => {
     for (const key in json.resources) {
-      return true
+      if (json.resources[key].following_id == followUserId && json.resources[key].follower_id == userid) {
+        return json.resources[key].id
+      }
     }
     return false
     // console.log('done')
@@ -78,38 +79,57 @@ async function IsFollowing (userid) {
   })
 }
 
-async function AddFollower (userid) {
-  const currentUserId = window.sessionStorage.getItem('user_id')
-  const data = {
-    follower_id: `${currentUserId}`,
-    following_id: `${userid}`
-  }
-  return fetch('http://localhost:5000/followers/', {
-    method: 'post',
-    body: JSON.stringify(data)
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json)
+async function addFollower () {
+  const followButtons = document.querySelectorAll('#followButton')
+  // console.log(followButtons)
+  if (followButtons) {
+    // console.log("here inside if followbuttons")
+    followButtons.forEach(item => {
+    // console.log("here inside for each")
+      item.addEventListener('click', async event => {
+        event.preventDefault()
+        // console.log("here inside click")
+        const followUserId = item.dataset.userid
+        const userId = window.sessionStorage.getItem('user_id')
+        if (!userId) {
+          window.location.href = "index.html"
+        }
+
+        const alreadyFollowing = await isFollowing(followUserId, userId)
+
+        if (alreadyFollowing) {
+          // console.log("already following" + alreadyFollowing)
+          removeFollower(alreadyFollowing)
+          item.classList.remove('liked')
+          item.classList.add('likeButton')
+        } else {
+          await fetch('http://localhost:5000/followers/', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              follower_id: userId,
+              following_id: followUserId
+            })
+          })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+          item.classList.add('liked')
+          item.classList.remove('likeButton')
+        }
+      })
     })
+  }
 }
 
-async function removeFollower (userId) {
-  const currentUserId = window.sessionStorage.getItem('user_id')
-  return fetch(`http://localhost:5000/followers/?follower_id=${currentUserId}&following_id=${userId}`, 
-  { method: 'get' })
+async function removeFollower (followers_id) {
+  fetch(`http://localhost:5000/followers/${followers_id}`, { method: 'delete' })
     .then((res) => res.json())
     .then((json) => {
       console.log(json)
-      for (const key in json.resources) {
-        if (json.resources[key].id) {
-          fetch(`http://localhost:5000/followers/${json.resources[key].id}`, { method: 'delete' })
-            .then((res) => res.json())
-            .then((json) => {
-              console.log(json)
-            })
-        }
-      }
     })
 }
 
@@ -145,8 +165,19 @@ async function displayPosts (userid, htmlElement) {
   for (const key in dataObj) {
     if (dataObj[key].user_id == userid) {
       const username = await getUserById(userid)
-       console.log(username)
-      htmlElement.innerHTML += `<article class="post"><div class="userId">User: ${username}</div><div class="postText">${dataObj[key].text}</div><div class="postTimestamp">${dataObj[key].timestamp}</div><div class="postButton"><button type="button" data-id="${dataObj[key].id}" class="likeButton" id="likeButton">Like</button></div></article>`
+      const currentUser = window.sessionStorage.getItem('user_id')
+      // console.log(username)
+
+      let likedOrNot = ""
+
+      if (await isLiked(dataObj[key].id, currentUser)) {
+        likedOrNot = "liked"
+      } else {
+        // console.log("here in the else")
+        likedOrNot = "likeButton"
+      }
+
+      htmlElement.innerHTML += `<article class="post"><div class="userId">User: ${username}</div><div class="postText">${dataObj[key].text}</div><div class="postTimestamp">${dataObj[key].timestamp}</div><div class="postButton"><button type="button" data-id="${dataObj[key].id}" class="${likedOrNot}" id="likeButton">Like</button></div></article>`
     }
   }
   likePost()
@@ -160,12 +191,38 @@ async function displayPublicPosts (url = 'http://localhost:5000/posts/') {
   // console.log(data.resources)
   for (const key in dataObj) {
     const username = await getUserById(dataObj[key].user_id)
+    const currentUser = window.sessionStorage.getItem('user_id')
+    let followingOrNot = ""
+
+    // console.log(dataObj[key].user_id + currentUser)
+
+    if (await isFollowing(dataObj[key].user_id, currentUser)) {
+      followingOrNot = "liked"
+    } else {
+      // console.log("here in the else")
+      followingOrNot = "likeButton"
+    }
+
+    let likedOrNot = ""
+
+    if (await isLiked(dataObj[key].id, currentUser)) {
+      likedOrNot = "liked"
+    } else {
+      // console.log("here in the else")
+      likedOrNot = "likeButton"
+    }
+
+    // console.log(dataObj[key].id + ": " + followingOrNot)
+
     publicDisplay.innerHTML += `<article class="post"><div class="userId">User: ${username}</div><div class="postText">
     ${dataObj[key].text}</div><div class="postTimestamp">${dataObj[key].timestamp}
     </div><div class="postButton"><button type="button" data-id="${dataObj[key].id}
-    " class="likeButton" id="likeButton">Like</button></div></article>`
+    " class="${likedOrNot}" id="likeButton">Like</button><button type="button" data-userid="${dataObj[key].user_id}
+    " class="${followingOrNot}" id="followButton">Follow</button></div></article>`
+    
   }
   likePost()
+  addFollower()
 }
 
 async function displayHomePosts (userid) {
@@ -187,14 +244,35 @@ async function displayHomePosts (userid) {
   for (const key in dataObj1) {
     if (dataObj1[key].user_id == followA[0] || dataObj1[key].user_id == followA[1]) {
       const username = await getUserById(dataObj1[key].user_id)
+      const currentUser = window.sessionStorage.getItem('user_id')
+      let followingOrNot = ""
+
+      if (await isFollowing(dataObj1[key].user_id, currentUser)) {
+        followingOrNot = "liked"
+      } else {
+        // console.log("here in the else")
+        followingOrNot = "likeButton"
+      }
+
+      let likedOrNot = ""
+
+      if (await isLiked(dataObj1[key].id, currentUser)) {
+        likedOrNot = "liked"
+      } else {
+        // console.log("here in the else")
+        likedOrNot = "likeButton"
+      }
+
       // console.log(username)
       homeDisplay.innerHTML += `<article class="post"><div class="userId">User: ${username}</div><div class="postText">
       ${dataObj1[key].text}</div><div class="postTimestamp">${dataObj1[key].timestamp}
       </div><div class="postButton"><button type="button" data-id="${dataObj1[key].id}
-      " class="likeButton" id="likeButton">Like</button></div></article>`
+      " class="${likedOrNot}" id="likeButton">Like</button><button type="button" data-userid="${dataObj1[key].user_id}
+      " class="${followingOrNot}" id="followButton">Follow</button></div></article>`
     }
   }
   likePost()
+  addFollower()
 }
 
 if (homeDisplay != null) {
@@ -256,7 +334,7 @@ export function CreateUser (user, pass, em) {
     })
 }
 
-// authenticate user data from login form on login button click
+// authenticate user data from create form on create button click
 if (document.getElementById('createButton')) {
   const loginButton = document.getElementById('createButton')
   loginButton.addEventListener('click', async event => {
@@ -348,6 +426,22 @@ if (document.getElementById('newPostButton')) {
 
 // add user likes
 
+async function isLiked (postId, userId) {
+  return fetch(`http://localhost:5000/likes/`, { method: 'get' })
+  .then((res) => res.json())
+  .then((json) => {
+    for (const key in json.resources) {
+      if (json.resources[key].post_id == postId && json.resources[key].user_id == userId) {
+        return json.resources[key].id
+      }
+    }
+    return false
+    // console.log('done')
+  }).catch((error) => {
+    throw error
+  })
+}
+
 async function likePost () {
   const likeButtons = document.querySelectorAll('#likeButton')
   // console.log(likeButtons)
@@ -371,26 +465,43 @@ async function likePost () {
           String(now.getUTCHours()).padStart(2, '0') + ':' +
           String(now.getUTCMinutes()).padStart(2, '0') + ':' +
           String(now.getUTCSeconds()).padStart(2, '0')
-        await fetch('http://localhost:5000/likes/', {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            post_id: postId,
-            timestamp: timestamp,
-            user_id: userId
+
+        const alreadyLiked = await isLiked(postId, userId)
+        
+        if (alreadyLiked) {
+          unLike(alreadyLiked)
+          item.classList.remove('liked')
+          item.classList.add('likeButton')
+        } else {
+          await fetch('http://localhost:5000/likes/', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              post_id: postId,
+              timestamp: timestamp,
+              user_id: userId
+            })
           })
-        })
-          .then(res => res.json())
-          .then(data => console.log(data))
-          .catch(err => console.log(err))
-        item.classList.add('liked')
-        item.classList.remove('likeButton')
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+          item.classList.add('liked')
+          item.classList.remove('likeButton')
+        }
       })
     })
   }
+}
+
+async function unLike (post_id) {
+  fetch(`http://localhost:5000/likes/${post_id}`, { method: 'delete' })
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json)
+    })
 }
 
 // display messages from api
